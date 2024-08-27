cf9b4ea737cde75d53a72eca2a0f1c3b0daf5a3a
Alfredo Lucas <alfredoliu14@live.com>
2024-08-27 10:32:19 -0300

    cria arquivos base para crud de task

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
